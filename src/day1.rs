ec3a5ba8c61a64c83cb141907214a938a625e812
iNViTiON <13241662+iNViTiON@users.noreply.github.com>
2024-12-01 15:25:02 +0000

    feat(day1): update input processing to use u32 and add similarity calculation for part 2

--- src/day1.rs
+++ src/day1.rs
@@ -1,3 +1,4 @@
+use std::collections::HashMap;
 use std::fs;
 
 struct Config {
@@ -15,25 +16,25 @@ impl Config {
   }
 }
 
-fn process_input(file_path: &str) -> (Vec<i32>, Vec<i32>) {
+fn process_input(file_path: &str) -> (Vec<u32>, Vec<u32>) {
   let input = fs::read_to_string(file_path).unwrap_or_else(|err| {
     eprintln!("Problem reading file: {}", err);
     std::process::exit(1);
   });
 
-  let mut input_left: Vec<i32> = Vec::new();
-  let mut input_right: Vec<i32> = Vec::new();
+  let mut input_left: Vec<u32> = Vec::new();
+  let mut input_right: Vec<u32> = Vec::new();
 
   for line in input.lines() {
     let mut line_iter = line.split_whitespace();
-    input_left.push(line_iter.next().unwrap().parse::<i32>().unwrap());
-    input_right.push(line_iter.next().unwrap().parse::<i32>().unwrap());
+    input_left.push(line_iter.next().unwrap().parse::<u32>().unwrap());
+    input_right.push(line_iter.next().unwrap().parse::<u32>().unwrap());
   }
 
   (input_left, input_right)
 }
 
-fn process_first(config: Config) -> u32 {
+fn process_first(config: &Config) -> u32 {
   let (mut input_left, mut input_right) = process_input(&config.in_file);
   input_left.sort();
   input_right.sort();
@@ -44,11 +45,31 @@ fn process_first(config: Config) -> u32 {
   let mut distances: Vec<u32> = Vec::new();
   for left in left_iter {
     let right = right_iter.next().unwrap();
-    let distance = right - left;
+    let distance = (*right as i32) - (*left as i32);
     distances.push(distance.abs() as u32);
   }
 
   distances.iter().sum::<u32>()
+}
+
+fn process_second(config: &Config) -> u32 {
+  let (input_left, input_right) = process_input(&config.in_file);
+  let mut map: HashMap<&u32, (u8, u8)> = HashMap::new();
+  for num in input_left.iter() {
+    match map.get_mut(num) {
+      Some((count, _)) => *count += 1,
+      None => { map.insert(num, (1, 0)); },
+    };
+  }
+  for num in input_right.iter() {
+    match map.get_mut(num) {
+      Some((_, count)) => *count += 1,
+      None => (),
+    };
+  }
+  map.into_iter().map(|(k, (l, r))| {
+    k * l as u32 * r as u32
+  }).sum()
 }
 
 pub fn run(mut args: impl Iterator<Item = String>) {
@@ -58,8 +79,11 @@ pub fn run(mut args: impl Iterator<Item = String>) {
   });
   println!("Input file: {}", config.in_file);
 
-  let distance = process_first(config);
+  let distance = process_first(&config);
   println!("Distance: {}", distance);
+
+  let similarity = process_second(&config);
+  println!("Similarity: {}", similarity);
 }
 
 #[cfg(test)]
@@ -71,7 +95,16 @@ mod tests {
     let config = Config {
       in_file: "input/day1_ex.txt".to_string(),
     };
-    let distance = process_first(config);
+    let distance = process_first(&config);
     assert_eq!(distance, 11);
+  }
+  
+  #[test]
+  fn test_process_second() {
+    let config = Config {
+      in_file: "input/day1_ex.txt".to_string(),
+    };
+    let similarity = process_second(&config);
+    assert_eq!(similarity, 31);
   }
 }
