fce63004670aeafa7c40556ba02267d6324e6800
iNViTiON <13241662+iNViTiON@users.noreply.github.com>
2024-12-01 14:47:20 +0000

    feat(day1): implement input processing and distance calculation

--- src/day1.rs
+++ src/day1.rs
@@ -15,6 +15,42 @@ impl Config {
   }
 }
 
+fn process_input(file_path: &str) -> (Vec<i32>, Vec<i32>) {
+  let input = fs::read_to_string(file_path).unwrap_or_else(|err| {
+    eprintln!("Problem reading file: {}", err);
+    std::process::exit(1);
+  });
+
+  let mut input_left: Vec<i32> = Vec::new();
+  let mut input_right: Vec<i32> = Vec::new();
+
+  for line in input.lines() {
+    let mut line_iter = line.split_whitespace();
+    input_left.push(line_iter.next().unwrap().parse::<i32>().unwrap());
+    input_right.push(line_iter.next().unwrap().parse::<i32>().unwrap());
+  }
+
+  (input_left, input_right)
+}
+
+fn process_first(config: Config) -> u32 {
+  let (mut input_left, mut input_right) = process_input(&config.in_file);
+  input_left.sort();
+  input_right.sort();
+
+  let left_iter = input_left.iter();
+  let mut right_iter = input_right.iter();
+
+  let mut distances: Vec<u32> = Vec::new();
+  for left in left_iter {
+    let right = right_iter.next().unwrap();
+    let distance = right - left;
+    distances.push(distance.abs() as u32);
+  }
+
+  distances.iter().sum::<u32>()
+}
+
 pub fn run(mut args: impl Iterator<Item = String>) {
   let config = Config::new(&mut args).unwrap_or_else(|err| {
     eprintln!("Problem parsing arguments: {}", err);
@@ -22,12 +58,20 @@ pub fn run(mut args: impl Iterator<Item = String>) {
   });
   println!("Input file: {}", config.in_file);
 
-  let input = fs::read_to_string(config.in_file).unwrap_or_else(|err| {
-    eprintln!("Problem reading file: {}", err);
-    std::process::exit(1);
-  });
+  let distance = process_first(config);
+  println!("Distance: {}", distance);
+}
 
-  for line in input.lines() {
-    println!("{}", line);
+#[cfg(test)]
+mod tests {
+  use super::*;
+
+  #[test]
+  fn test_process_first() {
+    let config = Config {
+      in_file: "input/day1_ex.txt".to_string(),
+    };
+    let distance = process_first(config);
+    assert_eq!(distance, 11);
   }
 }
