cba73a0295872c68506ed6330f6a8b664a102d00
iNViTiON <13241662+iNViTiON@users.noreply.github.com>
2024-12-03 17:09:55 +0000

    feat(day3): add part 2

--- src/day3.rs
+++ src/day3.rs
@@ -1,8 +1,14 @@
+use regex::{Captures, Match, Regex};
 use std::fs;
-use regex::Regex;
 
 struct Config {
     in_file: String,
+}
+
+#[derive(Debug)]
+struct Instruction {
+    enable: Option<bool>,
+    result: Option<usize>,
 }
 
 impl Config {
@@ -23,15 +29,62 @@ fn read_input_file(file_path: &str) -> String {
     })
 }
 
+fn to_num(data: Option<Option<Match<'_>>>) -> usize {
+    data.unwrap().unwrap().as_str().parse::<usize>().unwrap()
+}
+
 fn process_first(raw_dataset: &str) -> usize {
     let mul_regex = Regex::new(r"mul\((\d{1,3}),(\d{1,3})\)").unwrap();
-    mul_regex.captures_iter(raw_dataset).map(|capture| {
-        let mut iter = capture.iter();
-        iter.next();
-        let left = iter.next().unwrap().unwrap().as_str().parse::<usize>().unwrap();
-        let right = iter.next().unwrap().unwrap().as_str().parse::<usize>().unwrap();
-        left * right
-    }).sum()
+    mul_regex
+        .captures_iter(raw_dataset)
+        .map(|capture| {
+            let mut iter = capture.iter();
+            iter.next();
+            let left = to_num(iter.next());
+            let right = to_num(iter.next());
+            left * right
+        })
+        .sum()
+}
+
+fn to_instruction(capture: Captures<'_>) -> Instruction {
+    let mut iter = capture.iter();
+    let first = iter.next().unwrap().unwrap().as_str();
+    match first {
+        "do()" => Instruction {
+            enable: Some(true),
+            result: None,
+        },
+        "don't()" => Instruction {
+            enable: Some(false),
+            result: None,
+        },
+        _ => {
+            let left = to_num(iter.next());
+            let right = to_num(iter.next());
+            Instruction {
+                enable: None,
+                result: Some(left * right),
+            }
+        }
+    }
+}
+
+fn process_second(raw_dataset: &str) -> usize {
+    let mul_regex = Regex::new(r"mul\((\d{1,3}),(\d{1,3})\)|do\(\)|don't\(\)").unwrap();
+    let mut enable = true;
+    mul_regex
+        .captures_iter(raw_dataset)
+        .map(|capture| to_instruction(capture))
+        .filter(|instruction| match instruction.enable {
+            Some(value) => {
+                enable = value;
+                false
+            }
+            None => instruction.result.is_some() && enable,
+        })
+        .map(|instruction| instruction.result.unwrap())
+        .sum()
 }
 
 pub fn run(mut args: impl Iterator<Item = String>) {
@@ -42,8 +95,11 @@ pub fn run(mut args: impl Iterator<Item = String>) {
     println!("Input file: {}", config.in_file);
     let raw_dataset = read_input_file(&config.in_file);
 
-    let safe_count = process_first(&raw_dataset);
-    println!("Safe report count: {}", safe_count);
+    let mul_sum = process_first(&raw_dataset);
+    println!("Multiplication sum: {}", mul_sum);
+
+    let enabled_mul_sem = process_second(&raw_dataset);
+    println!("Enabled multiplication sum: {}", enabled_mul_sem);
 }
 
 #[cfg(test)]
@@ -51,8 +107,26 @@ mod tests {
     use super::*;
 
     #[test]
-    fn test_process_first() {
+    fn test_process_first_ex() {
         let raw_dataset = read_input_file("input/day3_ex.txt");
         assert_eq!(process_first(&raw_dataset), 161);
+    }
+
+    #[test]
+    fn test_process_first() {
+        let raw_dataset = read_input_file("input/day3.txt");
+        assert_eq!(process_first(&raw_dataset), 160672468);
+    }
+
+    #[test]
+    fn test_process_second_ex() {
+        let raw_dataset = read_input_file("input/day3_ex2.txt");
+        assert_eq!(process_second(&raw_dataset), 48);
+    }
+
+    #[test]
+    fn test_process_second() {
+        let raw_dataset = read_input_file("input/day3.txt");
+        assert_eq!(process_second(&raw_dataset), 84893551);
     }
 }
