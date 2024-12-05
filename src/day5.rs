16532c8ec76c69659a9df77e54185d1f93fae4d9
iNViTiON <13241662+iNViTiON@users.noreply.github.com>
2024-12-05 20:51:45 +0000

    feat(day5): second part

--- src/day5.rs
+++ src/day5.rs
@@ -1,5 +1,5 @@
 use regex_lite::Regex;
-use std::fs;
+use std::{cmp::Ordering, collections::HashSet, fs};
 
 struct Config {
     in_file: String,
@@ -55,18 +55,79 @@ fn to_ordering_regex_forbidden_rules(ordering_rules: &str) -> Regex {
     let builder = Regex::new(&regex_str).unwrap();
     builder
 }
-            
 
-fn process_first(raw_dataset: &str) -> usize {
-    let input = Input::new(raw_dataset);
+struct PartOneResult<'a> {
+    middle_page_sum: usize,
+    invalid_updates: Vec<&'a str>,
+}
+
+fn process_first<'a>(input: &'a Input) -> PartOneResult<'a> {
     let forbidden_rules = to_ordering_regex_forbidden_rules(input.ordering_rules);
-    let valid_updates = input.updates.lines().filter(|line| forbidden_rules.find(line).is_none());
+    let (valid_updates, invalid_updates): (Vec<&str>, Vec<&str>) = input
+        .updates
+        .lines()
+        .partition(|line| forbidden_rules.find(line).is_none());
     let middle_page_sum = valid_updates
+        .into_iter()
         .map(|line| {
             let pages = line.split(',').collect::<Vec<&str>>();
-            pages.get(pages.len() / 2).unwrap().parse::<usize>().unwrap()
-})
+            pages
+                .get(pages.len() / 2)
+                .unwrap()
+                .parse::<usize>()
+                .unwrap()
+        })
         .sum::<usize>();
+    PartOneResult {
+        middle_page_sum,
+        invalid_updates,
+    }
+}
+
+fn process_second(input: &Input, part_one_result: &PartOneResult) -> usize {
+    let ordering_pair = input
+        .ordering_rules
+        .lines()
+        .map(|line| {
+            let mut page_iter = line.split('|');
+            let first = page_iter.next().unwrap();
+            let latter = page_iter.next().unwrap();
+            (first, latter)
+        })
+        .collect::<Vec<(&str, &str)>>();
+    let middle_page_sum: usize = part_one_result
+        .invalid_updates
+        .iter()
+        .map(|line| {
+            let mut pages = line.split(',').collect::<Vec<&str>>();
+            let pages_set = pages.iter().collect::<HashSet<&&str>>();
+            let relevant_ordering_pair = ordering_pair
+                .iter()
+                .filter(|(first, latter)| pages_set.contains(first) && pages_set.contains(latter))
+                .copied()
+                .collect::<Vec<(&str, &str)>>();
+            pages.sort_by(|a, b| {
+                let rule = relevant_ordering_pair
+                    .iter()
+                    .find(|(first, latter)| first == a && latter == b || first == b && latter == a);
+                match rule {
+                    None => Ordering::Equal,
+                    Some((first, _)) => {
+                        if first == a {
+                            Ordering::Less
+                        } else {
+                            Ordering::Greater
+                        }
+                    }
+                }
+            });
+            pages
+                .get(pages.len() / 2)
+                .unwrap()
+                .parse::<usize>()
+                .unwrap()
+        })
+        .sum();
     middle_page_sum
 }
 
@@ -78,12 +139,16 @@ pub fn run(mut args: impl Iterator<Item = String>) {
     println!("Input file: {}", config.in_file);
 
     let raw_dataset = read_input_file(&config.in_file);
+    let input = Input::new(&raw_dataset);
 
-    let middle_page_sum = process_first(&raw_dataset);
-    println!("Valid update middle page sum: {}", middle_page_sum);
+    let part_one_result = process_first(&input);
+    println!("Valid update middle page sum: {}", part_one_result.middle_page_sum);
 
-    // let x_mas_count = process_second(&raw_dataset);
-    // println!("X-MAS count: {}", x_mas_count);
+    let middle_page_with_fix_sum = process_second(&input, &part_one_result);
+    println!(
+        "Valid update middle page with fixed sum: {}",
+        middle_page_with_fix_sum
+    );
 }
 
 #[cfg(test)]
@@ -91,20 +156,20 @@ mod tests {
     use super::*;
 
     #[test]
-    fn test_process_first_ex() {
+    fn test_process_ex() {
         let raw_dataset = read_input_file("input/day5_ex.txt");
-        assert_eq!(process_first(&raw_dataset), 143);
+        let input = Input::new(&raw_dataset);
+        let part_one_result = process_first(&input);
+        assert_eq!(part_one_result.middle_page_sum, 143);
+        assert_eq!(process_second(&input, &part_one_result), 123);
     }
 
-    // #[test]
-    // fn test_process_first() {
-    //     let raw_dataset = read_input_file("input/day4.txt");
-    //     assert_eq!(process_first(&raw_dataset), 2571);
-    // }
-
-    // #[test]
-    // fn test_process_second_ex() {
-    //     let raw_dataset = read_input_file("input/day4_ex.txt");
-    //     assert_eq!(process_second(&raw_dataset), 9);
-    // }
+    #[test]
+    fn test_process_first() {
+        let raw_dataset = read_input_file("input/day5.txt");
+        let input = Input::new(&raw_dataset);
+        let part_one_result = process_first(&input);
+        assert_eq!(part_one_result.middle_page_sum, 6384);
+        assert_eq!(process_second(&input, &part_one_result), 5353);
+    }
 }
