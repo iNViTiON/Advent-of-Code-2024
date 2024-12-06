f990f661e5d6347c6c27a9530e58bc697bdb9f1c
iNViTiON <13241662+iNViTiON@users.noreply.github.com>
2024-12-06 00:11:50 +0000

    feat(day5): optimize regex processing and enhance multithreading for updates

--- src/day5.rs
+++ src/day5.rs
@@ -1,4 +1,7 @@
 use regex_lite::Regex;
+use std::collections::VecDeque;
+use std::sync::{Arc, Mutex};
+use std::thread::{self, JoinHandle};
 use std::{cmp::Ordering, collections::HashSet, fs};
 
 struct Config {
@@ -16,7 +19,6 @@ impl Config {
     }
 }
 
-#[derive(Debug)]
 struct Input<'a> {
     ordering_rules: &'a str,
     updates: &'a str,
@@ -39,36 +41,84 @@ fn read_input_file(file_path: &str) -> String {
     })
 }
 
-fn to_ordering_regex_forbidden_rules(ordering_rules: &str) -> Regex {
-    let body = ordering_rules
-        .lines()
-        .map(|line| {
-            let mut page_iter = line.split('|');
-            let first = page_iter.next().unwrap();
-            let latter = page_iter.next().unwrap();
-            let rule_str = format!(r"(?:(^|,){},.*?,?{}(,|$))", latter, first);
-            rule_str
-        })
-        .collect::<Vec<String>>()
-        .join("|");
-    let regex_str = format!(r"(?:{})", body);
-    let builder = Regex::new(&regex_str).unwrap();
-    builder
+fn to_ordering_regex_forbidden_rules(ordering_rules: &str) -> Vec<Regex> {
+    let regex_combined_count = 10;
+    let mut ordering_rule_lines = ordering_rules.lines();
+    let mut regexes = Vec::new();
+    loop {
+        let mut processed_pair = Vec::new();
+        for _ in 0..regex_combined_count {
+            let line = ordering_rule_lines.next();
+            if let Some(line) = line {
+                let mut page_iter = line.split('|');
+                let first = page_iter.next().unwrap();
+                let latter = page_iter.next().unwrap();
+                let rule_str = format!(r"(?:(^|,){},.*?,?{}(,|$))", latter, first);
+                processed_pair.push(rule_str);
+            } else {
+                break;
+            }
+        }
+        let body = processed_pair.join("|");
+        let regex_str = format!(r"(?:{})", body);
+        let regex = Regex::new(&regex_str).unwrap();
+        regexes.push(regex);
+        if processed_pair.len() < regex_combined_count {
+            break;
+        }
+    }
+    regexes
 }
 
-struct PartOneResult<'a> {
+struct PartOneResult {
     middle_page_sum: usize,
-    invalid_updates: Vec<&'a str>,
+    invalid_updates: Vec<String>,
 }
 
-fn process_first<'a>(input: &'a Input) -> PartOneResult<'a> {
+fn process_first(input: &Input) -> PartOneResult {
     let forbidden_rules = to_ordering_regex_forbidden_rules(input.ordering_rules);
-    let (valid_updates, invalid_updates): (Vec<&str>, Vec<&str>) = input
+    let valid_updates = Arc::new(Mutex::new(Vec::new()));
+    let invalid_updates = Arc::new(Mutex::new(Vec::new()));
+    let mut handles: VecDeque<JoinHandle<()>> = VecDeque::new();
+
+    let updates: Vec<String> = input
         .updates
         .lines()
-        .partition(|line| forbidden_rules.find(line).is_none());
+        .map(|update| update.to_string())
+        .collect::<Vec<String>>();
+    for line in updates {
+        handles.retain(|handle| !handle.is_finished());
+        if handles.len() >= 8 {
+            let handle = handles.pop_front();
+            if let Some(handle) = handle {
+                handle.join().unwrap();
+            }
+        }
+        let forbidden_rules = forbidden_rules.clone();
+        let valid_updates = Arc::clone(&valid_updates);
+        let invalid_updates = Arc::clone(&invalid_updates);
+        let handle = thread::spawn(move || {
+            let mut is_valid = true;
+            for rule in forbidden_rules.into_iter() {
+                if rule.find(&line).is_some() {
+                    is_valid = false;
+                    break;
+                }
+            }
+            let store = if is_valid { valid_updates } else { invalid_updates };
+            let mut store = store.lock().unwrap();
+            store.push(line);
+        });
+        handles.push_back(handle);
+    }
+    for handle in handles {
+        handle.join().unwrap();
+    }
+
     let middle_page_sum = valid_updates
-        .into_iter()
+        .lock()
+        .unwrap()
+        .iter()
         .map(|line| {
             let pages = line.split(',').collect::<Vec<&str>>();
             pages
@@ -78,6 +128,12 @@ fn process_first<'a>(input: &'a Input) -> PartOneResult<'a> {
                 .unwrap()
         })
         .sum::<usize>();
+    let invalid_updates = invalid_updates
+        .lock()
+        .unwrap()
+        .iter()
+        .map(|s| s.to_string())
+        .collect::<Vec<String>>();
     PartOneResult {
         middle_page_sum,
         invalid_updates,
@@ -139,10 +195,18 @@ pub fn run(mut args: impl Iterator<Item = String>) {
     println!("Input file: {}", config.in_file);
 
     let raw_dataset = read_input_file(&config.in_file);
+
+    println!("WARNING! This is really slow, you can make it 10× faster by running with --release");
+    println!("e.g. `cargo run --release 4 input/day4.txt`");
+    println!("e.g. `cargo run -r 4 input/day4.txt`");
+
     let input = Input::new(&raw_dataset);
 
     let part_one_result = process_first(&input);
-    println!("Valid update middle page sum: {}", part_one_result.middle_page_sum);
+    println!(
+        "Valid update middle page sum: {}",
+        part_one_result.middle_page_sum
+    );
 
     let middle_page_with_fix_sum = process_second(&input, &part_one_result);
     println!(
@@ -165,7 +229,7 @@ mod tests {
     }
 
     #[test]
-    fn test_process_first() {
+    fn test_process() {
         let raw_dataset = read_input_file("input/day5.txt");
         let input = Input::new(&raw_dataset);
         let part_one_result = process_first(&input);
