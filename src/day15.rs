3b4afe436444909f7698b20bc18264ed80d0cea0
iNViTiON <13241662+iNViTiON@users.noreply.github.com>
2024-12-16 08:53:53 +0000

    feat(day15): second part

--- src/day15.rs
+++ src/day15.rs
@@ -17,6 +17,30 @@ impl Config {
     }
 }
 
+impl Position {
+    fn next_position(&self, direction: &char) -> Position {
+        match direction {
+            '>' => Position {
+                row: self.row,
+                col: self.col + 1,
+            },
+            '^' => Position {
+                row: self.row - 1,
+                col: self.col,
+            },
+            'v' => Position {
+                row: self.row + 1,
+                col: self.col,
+            },
+            '<' => Position {
+                row: self.row,
+                col: self.col - 1,
+            },
+            _ => panic!("Invalid direction"),
+        }
+    }
+}
+
 struct Map {
     robot: Position,
     map: HashMap<Position, bool>,
@@ -24,7 +48,7 @@ struct Map {
 
 impl Map {
     fn new(raw_map_dataset: &str) -> Self {
-        let size = raw_map_dataset.find("\n").unwrap();
+        let size = raw_map_dataset.find("\n").unwrap() + 1;
         let robot_offset = raw_map_dataset.find("@").unwrap();
         let robot = Position {
             col: (robot_offset % size) as u8,
@@ -94,25 +118,7 @@ impl Map {
     }
 
     fn move_robot(&mut self, direction: &char) {
-        let next_position = match direction {
-            '>' => Position {
-                row: self.robot.row,
-                col: self.robot.col + 1,
-            },
-            '^' => Position {
-                row: self.robot.row - 1,
-                col: self.robot.col,
-            },
-            'v' => Position {
-                row: self.robot.row + 1,
-                col: self.robot.col,
-            },
-            '<' => Position {
-                row: self.robot.row,
-                col: self.robot.col - 1,
-            },
-            _ => panic!("Invalid direction"),
-        };
+        let next_position = self.robot.next_position(direction);
         if self.clear_position(&next_position, direction) {
             self.robot = next_position;
         }
@@ -124,6 +130,137 @@ impl Map {
             .filter_map(|(position, movable)| match movable {
                 true => Some((position.row as u32 * 100) + position.col as u32),
                 false => None,
+            })
+            .sum()
+    }
+}
+
+struct MapDoubleWide {
+    robot: Position,
+    map: HashMap<Position, Option<bool>>,
+}
+
+impl MapDoubleWide {
+    fn new(raw_map_dataset: &str) -> Self {
+        let size = raw_map_dataset.find("\n").unwrap() + 1;
+        let robot_offset = raw_map_dataset.find("@").unwrap();
+        let robot = Position {
+            col: (robot_offset % size) as u8 * 2,
+            row: (robot_offset / size) as u8,
+        };
+
+        let lines = raw_map_dataset.lines();
+
+        let map = lines
+            .enumerate()
+            .flat_map(|(row, line)| {
+                line.chars().enumerate().map(move |(col, c)| {
+                    let position = Position {
+                        row: row as u8,
+                        col: col as u8 * 2,
+                    };
+                    (position, c)
+                })
+            })
+            .filter_map(|(position, c)| match c {
+                '#' => Some((position, false)),
+                'O' => Some((position, true)),
+                _ => None,
+            })
+            .fold(HashMap::new(), |mut acc, (position, movable)| {
+                let position_on_right = Position {
+                    row: position.row,
+                    col: position.col + 1,
+                };
+                let movable_option = match movable {
+                    true => Some(true),
+                    false => None,
+                };
+                let moveable_on_right_option = match movable {
+                    true => Some(false),
+                    false => None,
+                };
+                acc.insert(position, movable_option);
+                acc.insert(position_on_right, moveable_on_right_option);
+                acc
+            });
+
+        MapDoubleWide { robot, map }
+    }
+
+    fn can_move(&mut self, position: &Position, direction: &char) -> bool {
+        let current_position_data = self.map.get(position);
+        let another_position_of_box = match current_position_data {
+            None => {
+                return true;
+            }
+            Some(movable) => match movable {
+                None => {
+                    return false;
+                }
+                Some(true) => Position {
+                    row: position.row,
+                    col: position.col + 1,
+                },
+                Some(false) => Position {
+                    row: position.row,
+                    col: position.col - 1,
+                },
+            },
+        };
+        let next_position = position.next_position(direction);
+        let next_another_position_of_box = another_position_of_box.next_position(direction);
+        let self_can_move = self.can_move(&next_position, direction);
+        let another_can_move = next_another_position_of_box == *position
+            || self.can_move(&next_another_position_of_box, direction);
+        self_can_move && another_can_move
+    }
+
+    fn clear_position(&mut self, position: &Position, direction: &char) {
+        let current_position_data = self.map.get(position);
+        let another_position_of_box = match current_position_data {
+            None => {
+                return;
+            }
+            Some(movable) => match movable {
+                None => {
+                    return;
+                }
+                Some(true) => Position {
+                    row: position.row,
+                    col: position.col + 1,
+                },
+                Some(false) => Position {
+                    row: position.row,
+                    col: position.col - 1,
+                },
+            },
+        };
+        let current_position_data = self.map.remove(position).unwrap();
+        let another_position_of_box_data = self.map.remove(&another_position_of_box).unwrap();
+        let next_position = position.next_position(direction);
+        let next_another_position_of_box = another_position_of_box.next_position(direction);
+        self.clear_position(&next_position, direction);
+        self.clear_position(&next_another_position_of_box, direction);
+        self.map.insert(next_position, current_position_data);
+        self.map
+            .insert(next_another_position_of_box, another_position_of_box_data);
+    }
+
+    fn move_robot(&mut self, direction: &char) {
+        let next_position = self.robot.next_position(direction);
+        if self.can_move(&next_position, direction) {
+            self.clear_position(&next_position, direction);
+            self.robot = next_position;
+        }
+    }
+
+    fn get_box_sum_coordinate(&self) -> u32 {
+        self.map
+            .iter()
+            .filter_map(|(position, is_left)| match is_left {
+                Some(true) => Some((position.row as u32 * 100) + position.col as u32),
+                _ => None,
             })
             .sum()
     }
@@ -143,6 +280,13 @@ fn process_first(map: &mut Map, instructions: &str) -> u32 {
     map.get_box_sum_coordinate()
 }
 
+fn process_second(map: &mut MapDoubleWide, instructions: &str) -> u32 {
+    for instruction in instructions.chars() {
+        map.move_robot(&instruction);
+    }
+    map.get_box_sum_coordinate()
+}
+
 pub fn run(mut args: impl Iterator<Item = String>) {
     let config = Config::new(&mut args).unwrap_or_else(|err| {
         eprintln!("Problem parsing arguments: {}", err);
@@ -153,8 +297,16 @@ pub fn run(mut args: impl Iterator<Item = String>) {
     let raw_dataset = read_input_file(&config.in_file);
     let mut raw_dataset_split = raw_dataset.split("\n\n");
     let raw_map_dataset = raw_dataset_split.next().unwrap();
-    let raw_instruction_dataset = raw_dataset_split.next().unwrap().replace("\n", "");
+    let instructions = raw_dataset_split.next().unwrap().replace("\n", "");
+
     let mut map = Map::new(raw_map_dataset);
-    let box_coor_sum = process_first(&mut map, &raw_instruction_dataset);
+    let box_coor_sum = process_first(&mut map, &instructions);
     println!("Sum of all boxes final coordinates: {}", box_coor_sum);
+
+    let mut map_double_wide = MapDoubleWide::new(raw_map_dataset);
+    let box_coor_sum_double_wide = process_second(&mut map_double_wide, &instructions);
+    println!(
+        "Sum of all boxes final coordinates in second warehouse: {}",
+        box_coor_sum_double_wide
+    );
 }
